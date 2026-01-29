cd2ba48b53812de337a62e053ecd4c179f7812e7
aramayowil <aramayowil@gmail.com>
2026-01-29 15:31:07 -0300

    agregando suprabase

--- src/database/conection.database.ts
+++ src/database/conection.database.ts
@@ -1,12 +1,9 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
+dotenv.config()
 import { Pool } from 'pg'
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
 })
 
 try {
